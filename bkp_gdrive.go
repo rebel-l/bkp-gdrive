480af37324b2ec58a1d03d519941aa44858c2983
Lars Gaubisch <l.gaubisch@googlemail.com>
2021-11-07 03:30:53 +0100

    created main file

--- bkp_gdrive.go
+++
@@ -1,4 +0,0 @@
-// Package bkp_gdrive TODO: description of package
-package bkp_gdrive
-
-// TODO: add your code here
---
+++ main.go
@@ -0,0 +1,8 @@
+// Package main TODO: description of package
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello")
+}
